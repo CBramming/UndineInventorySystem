18f9dc8fe18e502582e021e7da9b494dd1344a06
ChristoZealand <chri23a7@edu.zealand.dk>
2023-11-22 12:01:24 +0100

    Test
    test to see if I can push code from stationær pc

--- undineinventorysystem/lib/widgets/login_widgets/login_screen_widgets.dart
+++ undineinventorysystem/lib/widgets/login_widgets/login_screen_widgets.dart
@@ -23,7 +23,7 @@ class UsernameInputField extends StatelessWidget {
     return TextFormField(
       controller: controller,
       decoration: const InputDecoration(
-        labelText: 'Email',
+        labelText: 'Email Test',
         suffixIcon: Icon(Icons.person_outline),
       ),
     );
