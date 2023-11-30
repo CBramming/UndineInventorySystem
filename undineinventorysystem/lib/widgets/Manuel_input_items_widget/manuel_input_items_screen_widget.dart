4c095cfae142c9a7e30760eb120bf8e03962aa61
ChristoZealand <chri23a7@edu.zealand.dk>
2023-11-30 20:15:53 +0100

    firebase rules n stuff

--- undineinventorysystem/lib/widgets/Manuel_input_items_widget/manuel_input_items_screen_widget.dart
+++ undineinventorysystem/lib/widgets/Manuel_input_items_widget/manuel_input_items_screen_widget.dart
@@ -1,22 +1,67 @@
 import 'package:flutter/material.dart';
 
-class InputItem extends StatelessWidget{
+class InputItem extends StatelessWidget {
   final TextEditingController inputitemscontroller;
 
   const InputItem({super.key, required this.inputitemscontroller});
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return TextFormField(
       controller: inputitemscontroller,
     );
   }
 }
-class DecrementButton extends StatelessWidget{
-  
+
+class DecrementButton extends StatelessWidget {
+  final Function() onPressed;
+
+  DecrementButton(this.onPressed);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text('Decrement'),
+    );
+  }
 }
 
-class IncrementButton extends StatelessWidget{
-  
+class IncrementButton extends StatelessWidget {
+  final Function() onPressed;
+
+  IncrementButton(this.onPressed);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text('Increment'),
+    );
+  }
+}
+
+class SendInputItem extends StatelessWidget {
+  final TextEditingController inputitemscontroller;
+  final int counter;
+  final void Function(String, int) onPressed;
+
+  SendInputItem({
+    required this.inputitemscontroller,
+    required this.counter,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        // Call the onPressed function with the input data
+        onPressed(inputitemscontroller.text, counter);
+      },
+      child: Text('Send Input Of Item'),
+    );
+  }
 }
 
 
