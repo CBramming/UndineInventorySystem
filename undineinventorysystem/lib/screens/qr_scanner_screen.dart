6dd2767f305bab5d628d41c994a3886273197c05
ChristoZealand <chri23a7@edu.zealand.dk>
2023-12-04 09:43:33 +0100

    idk scan

--- undineinventorysystem/lib/screens/qr_scanner_screen.dart
+++ undineinventorysystem/lib/screens/qr_scanner_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:undineinventorysystem/screens/Manuel_input_items_screen.dart';
 import 'package:undineinventorysystem/screens/login_screen.dart';
 import 'package:undineinventorysystem/widgets/qr_scanner_screen_widget/qr_scanner_widget.dart';
 
 class QRScannerScreen extends StatelessWidget {
   const QRScannerScreen({Key? key}) : super(key: key);
+
+  void handleScanResult(Barcode? result) {
+    // Handle the result as needed
+    if (result != null) {
+      // Do something with the scanned result
+      print('Scanned result: ${result.code}');
+    } else {
+      // Handle no result
+      print('No result');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,14 +31,31 @@ class QRScannerScreen extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: 
-        QrScannerButtonToManuelInputItems(
-          onForwardToManuelInputItems: () {
-            Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => const ManuelInputCounter()));
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            QRScannerWidget(onScanResult: handleScanResult),
+            const SizedBox(height: 20), // Adjust the height as needed
+            ElevatedButton(
+              onPressed: () {
+                // Handle the onPressed logic for the "Process Result" button
+              },
+              child: const Text('Process Result'),
+            ),
+            const SizedBox(height: 20), // Add additional spacing if necessary
+            QrScannerButtonToManuelInputItems(
+              onForwardToManuelInputItems: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ManuelInputCounter()),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+
